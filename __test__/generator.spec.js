5d38808c6f49eba64b8e6905ac016be301d42fe2
Döll, Sebastian <sebastian.doell@axelspringer.de>
2017-09-13 13:59:22 +0200

    ~ fixing linting

--- __test__/generator.spec.js
+++ __test__/generator.spec.js
@@ -2,23 +2,23 @@ import helpers from 'yeoman-test';
 import path from 'path';
 
 beforeEach(() => {
-    jest.resetModules();
-    // The object returned act like a promise, so return it to wait until the process is done
+  jest.resetModules();
+  // The object returned act like a promise, so return it to wait until the process is done
 });
 
 describe('run generator', () => {
-    test('generation', async () => {
-        try {
-            await helpers.run(path.join(__dirname, '../app'))
-                .withArguments([])
-                .withPrompts({
-                    "appName": "test",
-                    "name": "John Doe",
-                    "yarn": false
-                });
-        } catch (object) {
-            expect(object.error).toEqual('');
-        }
-    });
+  test('generation', async () => {
+    try {
+      await helpers.run(path.join(__dirname, '../app'))
+        .withArguments([])
+        .withPrompts({
+          "appName": "test",
+          "name": "John Doe",
+          "yarn": false
+        });
+    } catch (object) {
+      expect(object.error).toEqual('');
+    }
+  });
 })
 
